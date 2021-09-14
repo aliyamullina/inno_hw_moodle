28b6fdd18f5eeeb338e88bfb7b9c72bc017de75c
Aliya Mullina <aliya.mullina@ya.ru>
2021-09-14 10:45:50 +0300

    Режим headless для selenium.webdriver.chrome

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 from models.auth_model import AuthData
@@ -9,7 +10,22 @@ from pages.application import Application
 @pytest.fixture(scope='session')
 def app(request):
     base_url = request.config.getoption('--base-url')
-    fixture = Application(webdriver.Chrome(ChromeDriverManager().install()), base_url)
+    headless_mode = request.config.getoption("--headless").lower()
+
+    if headless_mode == "true":
+        chrome_options = Options()
+        chrome_options.headless = True
+        fixture = Application(
+            webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options),
+            base_url,
+        )
+    elif headless_mode == "false":
+        fixture = Application(
+            webdriver.Chrome(ChromeDriverManager().install()), base_url,
+        )
+    else:
+        raise pytest.UsageError("--headless should be true or false")
+
     yield fixture
     fixture.quit()
 
